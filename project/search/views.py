b38895f31c187a197bf9f2e40cd3ce68b9a81d58
Oleg <106367421+Olgert-A@users.noreply.github.com>
2022-09-28 16:59:59 +0500

    create template to show table

--- project/search/views.py
+++ project/search/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Detail, Assembly, Product, Binder
 
-def index(request):
-    return HttpResponse("Index page here")
+
+def index(request, detail_name):
+    search = Binder.objects.filter(detail__detail_name=detail_name)
+    active = [
+        (binder.assembly,
+         Product.objects.get(assembly=binder.assembly),
+         binder.amount)
+        for binder in search
+        if binder.status == 'Act'
+    ]
+
+    print(active)
+
+    context = {'detail': detail_name, 'active': active}
+    return render(request, 'search/index.html', context)
