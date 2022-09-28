14c757600734fb632019ecbf736a6394fd30a14c
Oleg <106367421+Olgert-A@users.noreply.github.com>
2022-09-28 20:43:30 +0500

    fix view template

--- project/search/views.py
+++ project/search/views.py
@@ -6,15 +6,7 @@ from .models import Detail, Assembly, Product, Binder
 
 def index(request, detail_name):
     search = Binder.objects.filter(detail__detail_name=detail_name)
-    active = [
-        (binder.assembly,
-         Product.objects.get(assembly=binder.assembly),
-         binder.amount)
-        for binder in search
-        if binder.status == 'Act'
-    ]
-
-    print(active)
-
-    context = {'detail': detail_name, 'active': active}
+    active = [binder for binder in search if binder.status == 'Act']
+    archive = [binder for binder in search if binder.status == 'Arc']
+    context = {'detail': detail_name, 'active': active, 'archive': archive}
     return render(request, 'search/index.html', context)
